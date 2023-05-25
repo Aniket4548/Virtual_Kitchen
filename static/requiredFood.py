1145548ce7c59db35556c21781af06b95ea5b814
Aniket Kumar <120406903+Aniket4548@users.noreply.github.com>
2023-05-25 21:26:28 +0530

    requiredFood.py

--- static/requiredFood.py
+++ static/requiredFood.py
@@ -5,7 +5,7 @@ import openai
 from gtts import gTTS
 
 # Set up OpenAI API credentials
-openai.api_key = "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"
+openai.api_key = "Your API Key"
 
 def chatbotFoodRequired(inputNoOfMembers,input_dishes):
     def textToSpeech(x):
