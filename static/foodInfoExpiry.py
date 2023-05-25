6a5bde867ec519fcd9b3da86d4576ac0aad06f53
Aniket Kumar <120406903+Aniket4548@users.noreply.github.com>
2023-05-25 21:53:11 +0530

    foodInfoExpiry.py

--- static/foodInfoExpiry.py
+++ static/foodInfoExpiry.py
@@ -3,7 +3,7 @@ import openai
 
 # Set up OpenAI API credentials
 def generated_text():
-    openai.api_key = "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"
+    openai.api_key = "Your API Key"
     
     #global query
     query =  input("enter the name of food: ")
